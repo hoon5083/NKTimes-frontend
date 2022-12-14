3c5bfb89c3c747d11458de765e75247577562f50
Yikanghoon <hoon5083@gmail.com>
2022-12-15 05:36:15 +0900

    feat(frontend): homevideocard 수정

--- src/components/pages/homeVideoCard.tsx
+++ src/components/pages/homeVideoCard.tsx
@@ -1,22 +1,25 @@
 import Link from "next/link";
-import { HTMLAttributes } from "react";
 
-interface Props extends HTMLAttributes<HTMLDivElement> {
-  title: string;
-  url: string;
-  route: string;
-}
-
-function HomeVideoCard({ title, route }: Props) {
+function HomeVideoCard() {
   return (
     <div className="flex flex-col w-full h-64 mx-0 mb-4 rounded-xl bg-cp-1">
       <div className="w-full px-3 border-b-2 border-black h-1/6">
-        <div className="flex justify-between align-bottom">
-          <p className="inline-block py-2 text-lg font-bold">영상 링크실</p>
-          <p className="inline-block py-2 text-sm"></p>
-        </div>
+        <Link href="/articles/3">
+          <div className="flex justify-between align-bottom">
+            <p className="inline-block py-1 text-lg font-bold">영상링크실</p>
+            <p className="inline-block py-2 text-sm">더보기</p>
+          </div>
+        </Link>
       </div>
-      <div></div>
+      <ul className="h-full px-2 list-none">
+        {/* <li
+          key="1"
+          className="flex flex-row justify-between px-1 py-1 border-b-2 border-black last:border-0"
+        >
+          <p>ㅁㄴㅇㄹ</p>
+          <p className="text-sm text-cp-5">ㅁㄴㅇㄹ</p>
+        </li> */}
+      </ul>
     </div>
   );
 }

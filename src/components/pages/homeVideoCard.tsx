dcb81d21063398684714ae35e4dc07a08d04e492
Yikanghoon <hoon5083@gmail.com>
2022-12-15 06:13:54 +0900

    feat(frontend): homevideocard 요청 라우트 수정

--- src/components/pages/homeVideoCard.tsx
+++ src/components/pages/homeVideoCard.tsx
@@ -4,7 +4,7 @@ function HomeVideoCard() {
   return (
     <div className="flex flex-col w-full h-64 mx-0 mb-4 rounded-xl bg-cp-1">
       <div className="w-full px-3 border-b-2 border-black h-1/6">
-        <Link href="/articles/3">
+        <Link href="/articles/4">
           <div className="flex justify-between align-bottom">
             <p className="inline-block py-1 text-lg font-bold">영상링크실</p>
             <p className="inline-block py-2 text-sm">더보기</p>

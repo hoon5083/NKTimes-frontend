228a9d90abc3ad887ae10956fe708108064571c6
Yikanghoon <hoon5083@gmail.com>
2022-12-19 07:37:51 +0900

    feat(frontend): app.tsx에 fontawesome 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import "styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/common/navbar";
 import Footer from "../components/common/footer";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

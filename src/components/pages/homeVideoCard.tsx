2b9ba0b71a388796a6db8afc966ec771912276e5
Yikanghoon <hoon5083@gmail.com>
2022-12-19 06:25:07 +0900

    feat(frontend): 영상 카드 컴포넌트 추가

--- src/components/pages/homeVideoCard.tsx
+++ src/components/pages/homeVideoCard.tsx
@@ -1,25 +1,21 @@
 import Link from "next/link";
+import { HTMLAttributes } from "react";
 
-function HomeVideoCard() {
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  order: number;
+}
+
+function HomeVideoCard({ order }: Props) {
   return (
-    <div className="flex flex-col w-full h-64 mx-0 mb-4 rounded-xl bg-cp-1">
+    <div className="flex flex-col w-full h-64 mx-2 mb-4 rounded-xl bg-cp-1">
       <div className="w-full px-3 border-b-2 border-black h-1/6">
         <Link href="/articles/4">
           <div className="flex justify-between align-bottom">
-            <p className="inline-block py-1 text-lg font-bold">영상링크실</p>
-            <p className="inline-block py-2 text-sm">더보기</p>
+            <p className="inline-block py-2 text-lg font-bold">최신 영상 {order}</p>
+            <p className="inline-block py-3 text-sm">더보기</p>
           </div>
         </Link>
       </div>
-      <ul className="h-full px-2 list-none">
-        {/* <li
-          key="1"
-          className="flex flex-row justify-between px-1 py-1 border-b-2 border-black last:border-0"
-        >
-          <p>ㅁㄴㅇㄹ</p>
-          <p className="text-sm text-cp-5">ㅁㄴㅇㄹ</p>
-        </li> */}
-      </ul>
     </div>
   );
 }

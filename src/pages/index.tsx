d755e63dd5f0691773bf7ebb0eb36b92f24fa613
Yikanghoon <hoon5083@gmail.com>
2022-12-19 06:31:07 +0900

    feat(frontend): 메인 페이지 레이아웃

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import type { NextPage } from "next";
 import HomeArticleCard from "../components/pages/homeArticleCard";
+import HomeInfoCard from "../components/pages/homeInfoCard";
+import HomeLunchCard from "../components/pages/homeLunchCard";
+import HomeMainCard from "../components/pages/homeMainCard";
+import HomeTalkingCard from "../components/pages/homeTalkingCard";
 import HomeVideoCard from "../components/pages/homeVideoCard";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <div className="grid w-11/12 grid-cols-1 mx-auto justify-items-center justify-self-center">
-        <HomeArticleCard title="공지" url="/articles/1" route="/articles/1" />
-        <HomeArticleCard title="기사" url="/articles/2" route="/articles/2" />
-        <HomeArticleCard title="자유게시판" url="/articles/3" route="/articles/3" />
-        <HomeVideoCard />
+      <div className="grid w-11/12 mx-auto sm:grid-cols-1 md:grid-cols-2 lg:w-3/4 xl:w-3/5 2xl: 2/5 justify-items-center justify-self-center gap-x-2 ">
+        <HomeMainCard />
+        <HomeVideoCard order={1} />
+        <HomeVideoCard order={2} />
+        <HomeArticleCard title="공지" url="/articles/1" route="/boards/1" />
+        <HomeArticleCard title="기사" url="/articles/2" route="/boards/2" />
+        <HomeArticleCard title="자유게시판" url="/articles/3" route="/boards/3" />
+        <HomeLunchCard />
+        <HomeTalkingCard />
+        <HomeInfoCard />
       </div>
     </div>
   );

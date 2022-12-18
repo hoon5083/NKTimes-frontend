9180b11bea58a323a9ca6df5138c460a62cbe01d
Yikanghoon <hoon5083@gmail.com>
2022-12-19 08:22:37 +0900

    feat(frontend): 게시판 드롭다운 추가

---
+++ src/components/common/boardDropdown.tsx
@@ -0,0 +1,21 @@
+import { Board, PagedApiResponse } from "../../types/api";
+import useSWR from "swr";
+import { authFetcher } from "../../utils/fetcher";
+import Link from "next/link";
+
+function BoardDropdown() {
+  const { data } = useSWR<PagedApiResponse<Board>>(`/boards?pageNumber=1&pageSize=50`, authFetcher);
+  return (
+    <div className="absolute grid w-screen grid-cols-4 p-10 h-fit bg-cp-5 top-12">
+      {data?.content.map((board, index: number) => {
+        return (
+          <Link key={index} href={"/boards/" + board.id}>
+            <div>{board.title}</div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
+
+export default BoardDropdown;
--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -4,6 +4,9 @@ import useSWR from "swr";
 import { authFetcher } from "../../utils/fetcher";
 import { User } from "../../types/api";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faList } from "@fortawesome/free-solid-svg-icons";
+import BoardDropdown from "./boardDropdown";
 
 function Navbar() {
   const { logout, loggedIn } = useGoogleAuth();
@@ -12,38 +15,41 @@ function Navbar() {
   const { data } = useSWR<User>(loggedIn ? "/users/me" : null, authFetcher);
 
   return (
-    <div className="flex justify-between w-full h-12 mb-5 bg-cp-4">
-      <div className="flex">
-        <Link href="/">
-          <p className="px-2 m-2 text-2xl">NKTimes</p>
-        </Link>
-        <button onClick={() => setIsDDOpen(!isDDOpen)}>
-          <div>O</div>
-        </button>
+    <>
+      <div className="flex justify-between w-full h-12 mb-5 bg-cp-4">
+        <div className="flex">
+          <Link href="/">
+            <p className="px-2 m-2 text-2xl">NKTimes</p>
+          </Link>
+          <button onClick={() => setIsDDOpen(!isDDOpen)}>
+            <FontAwesomeIcon icon={faList} />
+          </button>
+        </div>
+        <div>
+          <button
+            className="p-0 border-none outline-none cursor-pointer"
+            onClick={() => setIsLBOpen(!isLBOpen)}
+            id="auth_element_user-login-trigger"
+          >
+            <div className="z-50 w-48 h-12 p-3 bg-transparent rounded-lg">
+              {loggedIn ? data?.nickname : "Guest"}
+            </div>
+          </button>
+          {isLBOpen && (
+            <div className="justify-center w-48 rounded-md">
+              {loggedIn ? (
+                <div onClick={logout} className="flex justify-center">
+                  로그아웃
+                </div>
+              ) : (
+                <div id="auth_element_user-login-google"></div>
+              )}
+            </div>
+          )}
+        </div>
       </div>
-      <div>
-        <button
-          className="p-0 border-none outline-none cursor-pointer"
-          onClick={() => setIsLBOpen(!isLBOpen)}
-          id="auth_element_user-login-trigger"
-        >
-          <div className="z-50 w-48 h-12 p-3 bg-transparent rounded-lg">
-            {loggedIn ? data?.nickname : "Guest"}
-          </div>
-        </button>
-        {isLBOpen && (
-          <div className="justify-center w-48 rounded-md bg-cp-4">
-            {loggedIn ? (
-              <div onClick={logout} className="flex justify-center">
-                로그아웃
-              </div>
-            ) : (
-              <div id="auth_element_user-login-google"></div>
-            )}
-          </div>
-        )}
-      </div>
-    </div>
+      {isDDOpen ? <BoardDropdown /> : null}
+    </>
   );
 }
 export default Navbar;

b287f736d6301b5009ae6f1b00a9f3a63185ec44
Yikanghoon <hoon5083@gmail.com>
2022-12-20 03:26:20 +0900

    feat(backend): talkings page 추가

---
+++ src/pages/talkings.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from "next";
+import TalkingCard from "../components/pages/talkings/talkingCard";
+import { authFetcher } from "../utils/fetcher";
+import useSWR from "swr";
+import { PagedApiResponse, Talking } from "../types/api";
+import { useState } from "react";
+
+const Talkings: NextPage = () => {
+  const [pageCount, setPageCount] = useState(1);
+  const { data, mutate } = useSWR<PagedApiResponse<Talking>>(
+    `/talkings?pageNumber=1&pageSize=${pageCount * 20}`,
+    authFetcher
+  );
+  return (
+    <div className="flex flex-col w-11/12 h-screen mx-auto justify-s">
+      <div className="mx-auto my-10 text-6xl font-bold w-fit">담벼락</div>
+      {data?.content.map((talking, index: number) => {
+        const dateString =
+          talking.updatedAt === talking.createdAt
+            ? new Date(talking.createdAt).toLocaleString()
+            : new Date(talking.updatedAt).toLocaleString() + " (수정됨)";
+        return (
+          <TalkingCard
+            key={index}
+            authority={talking.author.authority}
+            dateString={dateString}
+            content={talking.content}
+            nickname={talking.author.nickname}
+          />
+        );
+      })}
+      {pageCount * 20 <= Number(data?.totalCount) ? (
+        <div className="" onClick={() => setPageCount(pageCount + 1)}>
+          더보기
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Talkings;

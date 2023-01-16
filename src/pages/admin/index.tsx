2df2114ed42af0efa1c3bd67313d7c09de106fd2
Yikanghoon <hoon5083@gmail.com>
2023-01-16 13:52:18 +0900

    feat(frontend): 게시판 관리 페이지 수정

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -5,15 +5,21 @@ const Admin: NextPage = () => {
   return (
     <div className="flex flex-col w-11/12 min-h-screen mx-auto justify-self-center justify-items-center">
       <div className="mx-auto my-10 text-6xl font-bold w-fit">관리페이지</div>
-      <Link href="/admin/boards">
-        <div>게시판 관리</div>
-      </Link>
-      <Link href="/admin/users">
-        <div>유저 관리</div>
-      </Link>
-      {/* <Link href="/admin/popups">
+      <div className="flex flex-col gap-2 ">
+        <Link href="/admin/boards">
+          <button className="p-1 px-2 text-white rounded-lg w-fit bg-cp-5 hover:shadow-xl">
+            게시판 관리
+          </button>
+        </Link>
+        <Link href="/admin/users">
+          <button className="p-1 px-2 text-white rounded-lg w-fit bg-cp-5 hover:shadow-xl">
+            유저 관리
+          </button>
+        </Link>
+        {/* <Link href="/admin/popups">
         <div>팝업 관리</div>
       </Link> */}
+      </div>
     </div>
   );
 };

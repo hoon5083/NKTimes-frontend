87d28b18dcce1d6a2349c48fedbeeddd6c7f1906
Yi Kanghoon <hoon5083@gmail.com>
2023-02-12 00:33:29 +0900

    feat: 메인 페이지 공지 overflow 문제 수정

--- src/components/pages/homeInfoCard.tsx
+++ src/components/pages/homeInfoCard.tsx
@@ -9,20 +9,15 @@ function HomeInfoCard() {
   const { data } = useSWR<ArticleDetails>(`/articles/1/${id}`, authFetcher);
   return (
     <Link href={`/articles/1/${id}`}>
-      <div className="flex flex-col w-full h-64 col-span-1 mx-2 mb-4 cursor-pointer md:col-span-2 rounded-xl bg-cp-1">
-        <div className="w-full px-3 border-b-2 border-black h-1/6">
-          <div className="flex justify-center">
-            <p className="inline-block py-2 text-lg font-bold text-center">공지사항</p>
-          </div>
-          <div className="flex flex-col">
-            <div className="self-center font-bold">{data?.title}</div>
-            <div className="inline-block py-3 text-sm">
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: String(data?.content),
-                }}
-              />
-            </div>
+      <div className="flex flex-col w-full h-64 col-span-1 p-2 mx-2 mb-4 cursor-pointer md:col-span-2 rounded-xl bg-cp-1">
+        <div className="flex flex-col overflow-auto">
+          <div className="self-center font-bold">{data?.title}</div>
+          <div className="py-3 text-sm">
+            <div
+              dangerouslySetInnerHTML={{
+                __html: String(data?.content),
+              }}
+            />
           </div>
         </div>
       </div>

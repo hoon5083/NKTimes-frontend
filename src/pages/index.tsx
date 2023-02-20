e0d99293c8d011f88c69c769d104f2dba3e7c86d
Yi Kanghoon <hoon5083@gmail.com>
2023-02-21 03:29:01 +0900

    feat: 메인 페이지 구조 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,17 @@ import HomeArticleCard from "../components/pages/homeArticleCard";
 import HomeInfoCard from "../components/pages/homeInfoCard";
 import HomeLoginCard from "../components/pages/homeLoginCard";
 import HomeMainArticleCard from "../components/pages/homeMainArticleCard";
-import HomeMainCard from "../components/pages/homeMainCard";
 import HomeVideoCard from "../components/pages/homeVideoCard";
+import MainBoardListCard from "../components/pages/MainBoardListCard";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <div className="grid w-[375px] mx-auto grid-cols-1 md:grid-cols-2 md:w-[750px] justify-items-center justify-self-center gap-x-4">
+      <div
+        className="grid w-[375px] mx-auto grid-cols-1 md:grid-cols-2 md:w-[750px] justify-items-center justify-self-center gap-x-4">
         <HomeInfoCard />
-        <HomeMainCard />
         <HomeMainArticleCard title="기사" url="/articles/2" route="/articles/2" />
+        <MainBoardListCard />
         <HomeVideoCard order={1} />
         <HomeVideoCard order={2} />
         <HomeArticleCard title="자유게시판" url="/articles/3" route="/articles/3" />

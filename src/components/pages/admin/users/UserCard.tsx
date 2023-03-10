b0d4ba0f8bf562d91e651f437451b9f12718f1f3
Yi Kanghoon <hoon5083@gmail.com>
2023-03-10 22:15:29 +0900

    refactor: UserInfo 적용

--- src/components/pages/admin/users/UserCard.tsx
+++ src/components/pages/admin/users/UserCard.tsx
@@ -1,7 +1,6 @@
 import { User } from "../../../../types/api";
-import { getAuthHeader } from "../../../../utils/auth";
-import { serverAxios } from "../../../../utils/commonAxios";
 import UserEditingForm from "./UserEditingForm";
+import UserInfo from "./UserInfo";
 
 interface Props {
   isDeleting: boolean;
@@ -14,73 +13,11 @@ interface Props {
 
 function UserCard({ isDeleting, isEditing, user, setDeletingNum, setEditingNum, mutate }: Props) {
 
-  const approveUser = (id: number) => {
-    const config = getAuthHeader(document.cookie);
-    serverAxios.patch(`/users/${id}`, { isApproved: true }, config);
-    location.reload();
-    mutate();
-  };
-
-  const deleteUser = (id: number) => {
-    const config = getAuthHeader(document.cookie);
-    serverAxios.delete(`/users/${id}`, config);
-    setDeletingNum(-1);
-    location.reload();
-    mutate();
-  };
-
   return (<div className="p-2 my-2 rounded-lg bg-cp-1">
     {isEditing ? (
       <UserEditingForm user={user} setEditingNum={setEditingNum} mutate={mutate} />
-    ) : (<div>
-        <div>별명: {user.nickname}</div>
-        <div>권한: {user.authority}</div>
-        {user.authority === "재학생" &&
-          <div>
-            <div>{user.grade}학년</div>
-            <div>{user.class}반</div>
-            <div>{user.studentId}번</div>
-          </div>
-        }
-        <div>실명: {user.name}</div>
-        <div>전화번호: {user.phone}</div>
-        <div>email: {user.email}</div>
-        {!user.isApproved &&
-          <button
-            onClick={() => {
-              approveUser(user.id);
-            }}
-            className="p-1 px-2 mr-2 text-white rounded-lg bg-cp-5 hover:shadow-xl"
-          >
-            승인
-          </button>}
-        {isDeleting ? (
-          <button
-            onClick={() => {
-              deleteUser(user.id);
-            }}
-            className="p-1 px-2 mr-2 text-white rounded-lg bg-cp-4 hover:shadow-xl"
-          >
-            확인
-          </button>
-        ) : (
-          <button
-            onClick={() => {
-              setDeletingNum(user.id);
-            }}
-            className="p-1 px-2 mr-2 text-white rounded-lg bg-cp-5 hover:shadow-xl"
-          >
-            삭제
-          </button>
-        )}
-        {!isEditing && <button
-          onClick={() => setEditingNum(user.id)}
-          className="p-1 px-2 mr-2 text-white rounded-lg bg-cp-5 hover:shadow-xl"
-        >
-          변경
-        </button>}
-      </div>
-
+    ) : (<UserInfo user={user} isDeleting={isDeleting} isEditing={isEditing} mutate={mutate}
+                   setDeletingNum={setDeletingNum} setEditingNum={setEditingNum} />
     )}
   </div>);
 }

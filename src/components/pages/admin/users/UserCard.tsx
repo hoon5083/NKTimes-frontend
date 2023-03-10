31e8fc9926792518f1c750eaba06df3dd741948c
Yi Kanghoon <hoon5083@gmail.com>
2023-03-10 21:49:32 +0900

    refactor: UserEditingForm 적용

--- src/components/pages/admin/users/UserCard.tsx
+++ src/components/pages/admin/users/UserCard.tsx
@@ -1,7 +1,7 @@
 import { User } from "../../../../types/api";
-import { FormEvent } from "react";
 import { getAuthHeader } from "../../../../utils/auth";
 import { serverAxios } from "../../../../utils/commonAxios";
+import UserEditingForm from "./UserEditingForm";
 
 interface Props {
   isDeleting: boolean;
@@ -29,67 +29,9 @@ function UserCard({ isDeleting, isEditing, user, setDeletingNum, setEditingNum, 
     mutate();
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    async function submitGroup() {
-      const form = e.currentTarget;
-      const formElements = form
-        ? (form.elements as typeof form.elements & {
-          authority: HTMLInputElement;
-        })
-        : null;
-      const config = getAuthHeader(document.cookie);
-      try {
-        const body = {
-          authority: formElements?.authority.value,
-        };
-        await serverAxios.patch(`/users/${user.id}`, body, config);
-        setEditingNum(-1);
-        mutate();
-      } catch (e) {
-        const error = e as any;
-        alert(error?.response?.data.message); //수정필요
-        console.log(e);
-      }
-    }
-
-    submitGroup();
-  };
-
   return (<div className="p-2 my-2 rounded-lg bg-cp-1">
     {isEditing ? (
-      <form onSubmit={handleSubmit} id="auth-update">
-        <div>별명: {user.nickname}</div>
-        <div>
-          권한:{" "}
-          <select name="authority" defaultValue={user.authority}>
-            <option value="교사">교사</option>
-            <option value="재학생">재학생</option>
-            <option value="졸업생">졸업생</option>
-            <option value="학생회">학생회</option>
-            <option value="방송반">방송반</option>
-            <option value="신문반">신문반</option>
-          </select>
-        </div>
-
-        {user.authority === "재학생" &&
-          <div>
-            <div>{user.grade}학년</div>
-            <div>{user.class}반</div>
-            <div>{user.studentId}번</div>
-          </div>
-        }
-        <div>실명: {user.name}</div>
-        <div>전화번호: {user.phone}</div>
-        <div>email: {user.email}</div>
-        <button
-          type="submit"
-          className="p-1 px-2 mr-2 text-white rounded-lg bg-cp-5 hover:shadow-xl"
-        >
-          저장
-        </button>
-      </form>
+      <UserEditingForm user={user} setEditingNum={setEditingNum} mutate={mutate} />
     ) : (<div>
         <div>별명: {user.nickname}</div>
         <div>권한: {user.authority}</div>

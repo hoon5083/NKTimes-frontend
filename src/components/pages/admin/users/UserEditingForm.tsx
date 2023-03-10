3f396aa34ecf96c9a6b85071f6b30673d50cd6c2
Yi Kanghoon <hoon5083@gmail.com>
2023-03-10 21:48:25 +0900

    refactor: UserEditingForm 분리

---
+++ src/components/pages/admin/users/UserEditingForm.tsx
@@ -0,0 +1,74 @@
+import { User } from "../../../../types/api";
+import { FormEvent } from "react";
+import { getAuthHeader } from "../../../../utils/auth";
+import { serverAxios } from "../../../../utils/commonAxios";
+
+interface Props {
+  user: User;
+  setEditingNum: (id: number) => void;
+  mutate: () => void;
+}
+
+function UserEditingForm({ user, setEditingNum, mutate }: Props) {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    async function submitGroup() {
+      const form = e.currentTarget;
+      const formElements = form
+        ? (form.elements as typeof form.elements & {
+          authority: HTMLInputElement;
+        })
+        : null;
+      const config = getAuthHeader(document.cookie);
+      try {
+        const body = {
+          authority: formElements?.authority.value,
+        };
+        await serverAxios.patch(`/users/${user.id}`, body, config);
+        setEditingNum(-1);
+        mutate();
+      } catch (e) {
+        const error = e as any;
+        alert(error?.response?.data.message); //수정필요
+        console.log(e);
+      }
+    }
+
+    submitGroup();
+  };
+
+  return <form onSubmit={handleSubmit} id="auth-update">
+    <div>별명: {user.nickname}</div>
+    <div>
+      권한:{" "}
+      <select name="authority" defaultValue={user.authority}>
+        <option value="교사">교사</option>
+        <option value="재학생">재학생</option>
+        <option value="졸업생">졸업생</option>
+        <option value="학생회">학생회</option>
+        <option value="방송반">방송반</option>
+        <option value="신문반">신문반</option>
+      </select>
+    </div>
+
+    {user.authority === "재학생" &&
+      <div>
+        <div>{user.grade}학년</div>
+        <div>{user.class}반</div>
+        <div>{user.studentId}번</div>
+      </div>
+    }
+    <div>실명: {user.name}</div>
+    <div>전화번호: {user.phone}</div>
+    <div>email: {user.email}</div>
+    <button
+      type="submit"
+      className="p-1 px-2 mr-2 text-white rounded-lg bg-cp-5 hover:shadow-xl"
+    >
+      저장
+    </button>
+  </form>;
+}
+
+export default UserEditingForm;
